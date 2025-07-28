02b931c34df927cde94a5a023b2c8ae8774ad20b
joshuakitong <joshuakitong@gmail.com>
2025-07-29 07:08:33 +0800

    Initial commit for Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white text-2xl">
+      Joshua Kitong Web Developer Portfolio
+    </div>
+  );
+}
+
+export default App;
