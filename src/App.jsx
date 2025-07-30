9b0a79d10d922f43b3c2ab723d6572cac2a4d448
joshuakitong <joshuakitong@gmail.com>
2025-07-31 00:21:36 +0800

    feat: add my svg art

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import StaticBackground from "./components/StaticBackground";
 import CustomCursor from "./components/CustomCursor";
+import MeSVGArt from "./components/MeSVGArt";
 
 function App() {
   return (
     <div className="relative min-h-screen bg-black text-white font-cascadia">
       <StaticBackground />
       <CustomCursor />
-      <main className="relative z-10 flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold">Hello! I'm Joshua Kitong</h1>
-        <p className="mt-4 text-gray-400">Front-End Web Developer</p>
+      <main className="relative p-6 z-10 flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold text-center">Hello! I'm Joshua Kitong</h1>
+        <p className="text-2xl mt-4 text-gray-300">Front-End Web Developer</p>
+        <MeSVGArt />
       </main>
     </div>
   );
