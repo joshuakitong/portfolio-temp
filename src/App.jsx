adbdec1957b64d901f9aa392ba2fc9ae57e55290
joshuakitong <joshuakitong@gmail.com>
2025-07-31 21:55:57 +0800

    docs: change web developer to front-end web developer

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         {/* Left Side */}
         <div className="w-full md:w-1/2 text-center md:text-left order-2 md:order-1">
           <h1 className="text-4xl md:text-5xl font-bold">Hello! I'm Joshua Kitong</h1>
-          <p className="text-3xl mt-4 text-blue-500">Web Developer</p>
+          <p className="text-3xl mt-4 text-blue-500">Front-End Web Developer</p>
           <p className="mt-4 text-base md:text-lg text-gray-200 leading-relaxed">
             I'm a versatile web developer with a solid foundation in front-end development with over 3 years of professional experience building responsive and performant web applications.
             I specialize in modern JavaScript frameworks like React, Next.js, Vue.js, and Nuxt.js, and I'm experienced with back-end tools like Node.js and Firebase to create full-stack solutions.
