4a9ecf910a82bfb405ef6d714aaa4af64548bd22
joshuakitong <joshuakitong@gmail.com>
2025-08-04 04:41:24 +0800

    refactor: adjust section padding and apply margin on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,16 @@ function App() {
       <div id="home" className="relative z-10 flex flex-col md:flex-row items-center justify-between min-h-screen max-w-[1600px] mx-auto pt-20 md:pt-0 px-6">
         <Home />
       </div>
-      <div id="about" className="relative z-10 items-center justify-between min-h-screen/2 max-w-4xl mx-auto pt-24 px-6" >
+      <div id="about" className="relative z-10 items-center justify-between min-h-screen/2 max-w-4xl mx-auto scroll-mt-12 pt-12 px-6" >
         <About />
       </div>
-      <div id="skills" className="relative z-10 items-center justify-between min-h-screen/2 max-w-[1600px] mx-auto pt-24 px-6" >
+      <div id="skills" className="relative z-10 items-center justify-between min-h-screen/2 max-w-[1600px] mx-auto scroll-mt-12 pt-12 px-6" >
         <Skills />
       </div>
-      <div id="projects" className="relative z-10 items-center justify-between min-h-screen max-w-4xl mx-auto pt-24 px-6">
+      <div id="projects" className="relative z-10 items-center justify-between min-h-screen max-w-4xl mx-auto scroll-mt-12 pt-12 px-6">
         <Projects />
       </div>
-      <div id="contact" className="relative z-10 items-center justify-between min-h-screen/2 max-w-md mx-auto pt-24 px-6" >
+      <div id="contact" className="relative z-10 items-center justify-between min-h-screen/2 max-w-md mx-auto scroll-mt-12 pt-12 px-6" >
         <Contact />
       </div>
       <footer className="text-sm text-white/50 text-center pt-24 pb-6">&copy; Joshua Kitong 2025. All Rights Reserved</footer>
